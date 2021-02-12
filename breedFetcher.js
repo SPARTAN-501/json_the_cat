5a1172eaebe2c93f378f9ef469a518abffdecd2a
SPARTAN-501 <luke.yin@alumni.ubc.ca>
2021-02-12 04:37:56 +0000

    Finally completed the cat test

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,31 @@
 const request = require('request');
-const args = process.argv.slice(2);
-request('https://api.thecatapi.com/v1/breeds/search?q=' + args, (error, response, body) => {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received]
-  console.log(typeof body);
-  const data = JSON.parse(body);
-  console.log(data);
-  console.log(typeof data);
-  if (data [0] !== undefined) {
-    console.log(data[0]);
-  } else if (data[0] === undefined) {
-    console.log("Breed not found");
-  }
-  // console.log('body:', body); // Print the HTML for the Google homepage.
-});
+
+const fetchBreedDescription = function(breedName, callback) {
+  
+  request('https://api.thecatapi.com/v1/breeds/search?q=' + breedName, (error, response, body) => {
+    /*
+    console.log('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received]
+    console.log(typeof body);
+    */
+    
+    if (error) {
+      callback(error, null);
+    }
+
+    const data = JSON.parse(body);
+    /*
+    console.log(data);
+    console.log(typeof data);
+    */
+    if (data.length > 0) {
+      console.log(data[0].description);
+      callback(null, data[0].description);
+    } else {
+      callback(null, "Breed not found");
+    }
+    // console.log('body:', body); // Print the HTML for the Google homepage.
+  });
+};
+
+module.exports = { fetchBreedDescription };
